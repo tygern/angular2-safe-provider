68049e5d69e6dafe790f504d94726db3ed03d260
Tyson Gern <tygern@gmail.com>
2016-05-01 09:28:25 +0100

    Add useValue and useFactory functions

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,16 @@ import {provide, OpaqueToken, Provider} from "angular2/core";
 export function safeProvide<T>(token:OpaqueToken) {
 
     return {
-        useClass<U extends T>(Klass:{new(...args:any[]):U;}):Provider {
+        useClass<U extends T>(Klass:{new(...args:any[]):U;}): Provider {
             return provide(token, {useClass: Klass});
+        },
+
+        useValue<U extends T>(value:U): Provider {
+            return provide(token, {useValue: value});
+        },
+
+        useFactory<U extends T>(factory:(...args:any[]) => U): Provider {
+            return provide(token, {useFactory: factory});
         }
     };
 }
